04335c8bc01a7101ed1c342a423c811fe3770b7b
dmpierre <pdaixmoreux@gmail.com>
2023-09-03 15:51:05 +0200

    chore: add link to post for devs

--- packages/ui/Description.tsx
+++ packages/ui/Description.tsx
@@ -6,7 +6,7 @@ export const Description: React.FC<{ total: number, type: string, iteration_coun
             <p>
                 This is an app aggregating {total} ECDSA signatures - {iteration_count} folding steps, {per_iteration_count} signatures per step - over secp/secq using Nova. You can test it to generate and subsequently verify a compressed Nova SNARK.
                 <br /> <br />
-                Read <a className="underline text-violet-500" target="_blank" href="">here</a> on how to leverage Nova within your zk app.
+                Read <a className="underline text-violet-500" target="_blank" href="https://hackmd.io/KxG-BH1nQPGpdRxz6M50hw?view">here</a> on how to leverage Nova within your zk app.
                 And <a className="underline text-violet-500" target="_blank" href="https://hackmd.io/@PierreDM/rkbPW-AT2">here</a> on how we did this along with some benchmarks.
             </p>
         </div>
